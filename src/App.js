9736e5c3260f53ce7c23090d7d5466ecd1aa2827
Tatsuya Asami <sharekobekko@gmail.com>
2018-09-15 13:26:33 +0900

    import AddTodo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import AddTodo from './components/addTodo/';
 
 const App = () => (
-  <div>My App</div>
+  <div>
+    <h1>Todo list</h1>
+    <AddTodo />
+  </div>
 );
 
 export default App;
